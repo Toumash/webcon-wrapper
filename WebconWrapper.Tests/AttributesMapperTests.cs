ec3e7b1da473c5ebef7d7872a70577f29fc38665
toumash <mr.toumash@gmail.com>
2018-07-06 21:01:00 +0200

    adds moq and attribute mapper

---
+++ WebconWrapper.Tests/AttributesMapperTests.cs
@@ -0,0 +1,176 @@
+using Moq;
+using System;
+using WebconProxy;
+using WebconWrapper;
+using Xunit;
+using System.Collections.Generic;
+
+namespace WebconWrapper.Tests
+{
+    public class AttributesMapperTests : IDisposable
+    {
+        private MockRepository mockRepository;
+
+        public AttributesMapperTests()
+        {
+            this.mockRepository = new MockRepository(MockBehavior.Strict);
+        }
+
+        public void Dispose()
+        {
+            this.mockRepository.VerifyAll();
+        }
+
+        private NewElement InitializeEmptyNewElement(NewElement element = null)
+        {
+            if (element == null)
+                element = new NewElement();
+
+            element.BoolAttributes = new List<BoolAttribute>();
+            element.ChooseAttributes = new List<ChooseAttribute>();
+            element.DateTimeAttributes = new List<DateTimeAttribute>();
+            element.DecimalAttributes = new List<DecimalAttribute>();
+            element.HyperLinkAttributes = new List<HyperLinkAttribute>();
+            element.IntegerAttributes = new List<IntegerAttribute>();
+            element.PeopleAttributes = new List<PeopleAttribute>();
+            element.PickerAttributes = new List<PickerAttribute>();
+            element.SubElementAttributes = new List<SubelementAttribute>();
+            element.TextAttributes = new List<TextAttribute>();
+            element.TreeAttributes = new List<TreeAttribute>();
+            element.CommentAttributes = new List<CommentAttribute>();
+            return element;
+        }
+
+        private AttributesMapper CreateAttributesMapper()
+        {
+            return new AttributesMapper();
+        }
+
+        [Fact]
+        public void SetValue_SetsIntegerAttribute()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new IntegerAttribute() { Id = id };
+            element.IntegerAttributes.Add(att);
+            int value = 150;
+
+            // Act
+            unitUnderTest.SetValue(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value);
+        }
+
+        [Fact]
+        public void SetValue_SetsBooleanAttribute()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new BoolAttribute() { Id = id };
+            element.BoolAttributes.Add(att);
+            bool value = true;
+
+            // Act
+            unitUnderTest.SetValue(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value);
+        }
+
+        [Fact]
+        public void SetValue_SetsStringAttribute()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new TextAttribute() { Id = id };
+            element.TextAttributes.Add(att);
+            string value = "test string";
+
+            // Act
+            unitUnderTest.SetValue(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value);
+        }
+
+        [Fact]
+        public void SetValue_SetsDateTimeAttribute()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new DateTimeAttribute() { Id = id };
+            element.DateTimeAttributes.Add(att);
+            DateTime value = DateTime.Now;
+
+            // Act
+            unitUnderTest.SetValue(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value);
+        }
+
+        [Fact]
+        public void SetValue_SetsDecimalAttribute()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new DecimalAttribute() { Id = id };
+            element.DecimalAttributes.Add(att);
+            decimal value = 15.15m;
+
+            // Act
+            unitUnderTest.SetValue(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value);
+        }
+
+        [Fact]
+        public void AddComment_AddsComment()
+        {
+            // Arrange
+            var unitUnderTest = CreateAttributesMapper();
+            NewElement element = InitializeEmptyNewElement();
+            int id = 15;
+            var att = new CommentAttribute() { Id = id };
+            att.Value = new Comments();
+            element.CommentAttributes.Add(att);
+            string value = "test string";
+
+            // Act
+            unitUnderTest.AddComment(
+                element,
+                id,
+                value);
+
+            // Assert
+            Assert.Equal(value, att.Value.NewComment);
+        }
+    }
+}
---
+++ WebconWrapper/AttributesMapper.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebconProxy;
+
+namespace WebconWrapper
+{
+    public class AttributesMapper
+    {
+        public void SetValue(NewElement element, int id, int value)
+            => GetAttributeById<IntegerAttribute>(element, id).Value = value;
+
+        public void SetValue(NewElement element, int id, string value)
+            => GetAttributeById<TextAttribute>(element, id).Value = value;
+
+        public void SetValue(NewElement element, int id, bool value)
+            => GetAttributeById<BoolAttribute>(element, id).Value = value;
+
+        public void SetValue(NewElement element, int id, DateTime value)
+            => GetAttributeById<DateTimeAttribute>(element, id).Value = value;
+
+        public void SetValue(NewElement element, int id, decimal value)
+            => GetAttributeById<DecimalAttribute>(element, id).Value = value;
+
+        public void AddComment(NewElement element, int id, string value)
+            => GetAttributeById<CommentAttribute>(element, id).Value.NewComment = value;
+
+
+
+        internal T GetAttributeById<T>(NewElement element, int id)
+            where T : WebconProxy.Attribute
+            => GetAttribute<T>(element, (a) => a.Id == id);
+
+        internal T GetAttributeByName<T>(NewElement element, string fieldName)
+            where T : WebconProxy.Attribute
+            => GetAttribute<T>(element, (a) => a.FieldName == fieldName);
+
+        private T GetAttribute<T>(NewElement element, Func<WebconProxy.Attribute, bool> selector)
+            where T : WebconProxy.Attribute
+        {
+            var attributes = new List<WebconProxy.Attribute>();
+            attributes.AddRange(element.BoolAttributes);
+            attributes.AddRange(element.ChooseAttributes);
+            attributes.AddRange(element.CommentAttributes);
+            attributes.AddRange(element.DateTimeAttributes);
+            attributes.AddRange(element.DecimalAttributes);
+            attributes.AddRange(element.HyperLinkAttributes);
+            attributes.AddRange(element.IntegerAttributes);
+            attributes.AddRange(element.PeopleAttributes);
+            attributes.AddRange(element.PickerAttributes);
+            attributes.AddRange(element.SubElementAttributes);
+            attributes.AddRange(element.TextAttributes);
+            attributes.AddRange(element.TreeAttributes);
+            var att = attributes.First(selector);
+            return att as T;
+        }
+    }
+}
