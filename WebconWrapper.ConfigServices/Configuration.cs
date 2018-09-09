153724181bfe31620f98603c13abee0b4adc864f
toumash <mr.toumash@gmail.com>
2018-09-09 23:34:34 +0200

    adds sql files into the project

---
+++ WebconWrapper.ConfigServices/Configuration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebconWrapper.ConfigServices
+{
+    public class Configuration
+    {
+        private static Configuration _instance;
+        public static Configuration Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Configuration();
+                return _instance;
+            }
+        }
+
+        public string AllAttributesQuery => ConfigServices.AllAttritubes;
+    }
+}
