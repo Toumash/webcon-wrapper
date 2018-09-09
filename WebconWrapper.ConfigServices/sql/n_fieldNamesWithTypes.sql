2ac0cabfb511b5650a5767c6a81098681d711ff3
Tomasz Dłuski <mr.toumash@gmail.com>
2018-09-09 23:04:46 +0200

    removes tops from selects

--- WebconWrapper.ConfigServices/sql/n_fieldNamesWithTypes.sql
+++ WebconWrapper.ConfigServices/sql/n_fieldNamesWithTypes.sql
@@ -14,7 +14,7 @@ SELECT
       , d.EnglishName
 FROM [BPS_Content].[dbo].[WFConfigurations] c
     INNER JOIN
-    (SELECT TOP (1000)
+    (SELECT
         [TypeID]
       , [Name]
       , [ObjectName]
@@ -24,12 +24,12 @@ FROM [BPS_Content].[dbo].[WFConfigurations] c
 d
     ON d.TypeID = c.WFCON_FieldTypeID
     LEFT JOIN
-    (SELECT TOP (1000)
-        [WF_ID]
-      , [WF_WFDEFID]
-      , [WF_Name]
+    (SELECT
+        -- [WF_ID],
+        [WF_WFDEFID],
+        [WF_Name]
     FROM [BPS_Content].[dbo].[WorkFlows])
 wf
     ON wf.WF_WFDEFID = c.WFCON_DEFID
-where c.WFCON_IsDeleted = 0
+where  c.WFCON_IsDeleted = 0
 order by WF_Name
