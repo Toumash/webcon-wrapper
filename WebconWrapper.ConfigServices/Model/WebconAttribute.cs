8b48569e10462a7c21e13a578073ce4e68d56254
toumash <mr.toumash@gmail.com>
2018-09-09 23:35:15 +0200

    adds repository reading all attributes into the model (not tested)

---
+++ WebconWrapper.ConfigServices/Model/WebconAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace WebconWrapper.ConfigServices.Model
+{
+    public class WebconAttribute
+    {
+        public long Id { get; set; }
+        public string WFName { get; set; }
+        public long DEFID { get; set; }
+        public string Title { get; set; }
+        public string Type { get; set; }
+        public long TypeId { get; set; }
+    }
+}
---
+++ WebconWrapper.ConfigServices/Services/AttributesRepo.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebconWrapper.ConfigServices.Model;
+
+namespace WebconWrapper.ConfigServices.Services
+{
+    public class AttributesRepo
+    {
+        private readonly string _connectionString;
+
+        public AttributesRepo(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public ICollection<WebconAttribute> GetAll()
+        {
+            var data = GetAttributesData();
+            ICollection<WebconAttribute> attributesInfo = MapToAttributesInfo(data);
+            return attributesInfo;
+        }
+
+        private static ICollection<WebconAttribute> MapToAttributesInfo(DataTable data)
+        {
+            return data.Rows.Cast<DataRow>().Select(row =>
+            new WebconAttribute()
+            {
+                Id = row.Field<int>("A_Id"),
+                WFName = row.Field<string>("WF_Name"),
+                DEFID = row.Field<int>("DEFID"),
+                Title = row.Field<string>("A_Title"),
+                Type = row.Field<string>("A_Type"),
+                TypeId = row.Field<int>("A_TypeId")
+            }).ToList();
+        }
+
+        private DataTable GetAttributesData()
+        {
+            using (var conn = CreateConnection())
+            using (var cmd = new SqlCommand(Configuration.Instance.AllAttributesQuery, conn))
+            using (var da = new SqlDataAdapter(cmd))
+            {
+                var dt = new DataTable();
+                da.Fill(dt);
+                return dt;
+            }
+        }
+
+        private SqlConnection CreateConnection()
+        {
+            var conn = new SqlConnection(_connectionString);
+            conn.Open();
+            return conn;
+        }
+    }
+}
